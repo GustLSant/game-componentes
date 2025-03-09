ff17f9dec2706d7680e0588bd66e56e8733d0cd9
Gustavo LS <gustlsantdev@gmail.com>
2025-03-09 20:33:51 -0300

    add: camera side control

--- Components/ThirdPersonShooterPlayer/Camera.gd
+++ Components/ThirdPersonShooterPlayer/Camera.gd
@@ -6,6 +6,8 @@ extends Node3D
 @onready var raycast:RayCast3D = $PivotHeight/PivotRot/RayCast3D
 @onready var camera:Camera3D = $PivotHeight/PivotRot/Camera3D
 
+var cameraSide:int = 1
+
 
 func _ready()->void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -21,12 +23,23 @@ func _input(_event:InputEvent)->void:
 	pass
 
 
-func _process(delta:float)->void:
+func _process(_delta:float)->void:
+	handleCameraCollision()
+	handleCameraSide(_delta)
+	return
+
+
+func handleCameraCollision()->void:
 	if(raycast.is_colliding()):
 		var vecToCollPoint = raycast.get_collision_point() - raycast.global_position
 		camera.global_position = raycast.get_collision_point() - vecToCollPoint.normalized()*0.1
 	else:
 		camera.global_position = raycast.global_position + raycast.global_transform.basis.z * raycast.target_position.z
 		pass
-	
-	return
+	pass
+
+
+func handleCameraSide(_delta:float)->void:
+	raycast.rotation_degrees.y = lerp(raycast.rotation_degrees.y, 15.0*cameraSide, 8*_delta)
+	if(Input.is_action_just_pressed("ToggleCameraSide")): cameraSide *= -1
+	pass
